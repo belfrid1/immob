70d9a7511d54b0c11f76564a4f95d4bbe9e3d75f
KAKANAKOU <belfrid1@gmail.com>
2021-05-25 22:26:49 +0200

    nom not null

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,17 +5,23 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
-    
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
     public function load(ObjectManager $manager)
     {
         
         for ($count = 0; $count < 20; $count++) {
             $user = new User();
             $user->setLogin("Login " . $count);
-            $user->setPrenom("nom" . $count);
+            $user->setNom("nom" . $count);
             $user->setPrenom("Prenom" . $count);
             
             $password = $this->encoder->encodePassword($user, 'password');
