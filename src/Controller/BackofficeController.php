e26c3d9790e06652535342588d6717e380a765d9
KAKANAKOU <belfrid1@gmail.com>
2021-06-01 20:45:25 +0200

    form

--- src/Controller/BackofficeController.php
+++ src/Controller/BackofficeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Annonce;
+use App\Entity\Bien;
+use App\Entity\Contrat;
+use App\Entity\User;
+use App\Repository\BienRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,9 +20,25 @@ class BackofficeController extends AbstractController
     {
 
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $biens = $this->getDoctrine()
+            ->getRepository(Bien::class)
+            ->findAll();
+
+            $annonces = $this->getDoctrine()
+            ->getRepository(Annonce::class)
+            ->findAll();
+            $utilisateurs = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findAll();
+            $contrats = $this->getDoctrine()
+            ->getRepository(Contrat::class)
+            ->findAll();
 
         return $this->render('backoffice/index.html.twig', [
-            'controller_name' => 'BackofficeController',
+            'biens' =>$biens,
+            'annonces' =>$annonces,
+            'utilisateurs' =>$utilisateurs,
+            'contrats' =>$contrats,
         ]);
     }
 }
